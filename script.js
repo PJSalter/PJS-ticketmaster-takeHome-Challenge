8df42418ca094b06fab13946fd138bdfaa48d2f0
PJSalter <psalter88@googlemail.com>
2022-03-13 00:40:48 +0000

    email and phone regex inside functions, validating both with messages

--- script.js
+++ script.js
@@ -29,9 +29,78 @@ const correctInfo = input => popupMessage(input, "", true);
 
 console.log(correctInfo)
 
-const includeTheValue = (input, message) => input.value.trim() === "" ? appearingError(input, message) : correctInfo(input)
+// setting the value onto these inputs.
+const includeTheValue = (input, message) => {
 
+if(input.value.trim() === "") {
+    return appearingError(input, message) 
+} else {
+    return correctInfo(input)
+}
+}
 //console.log(input)
 
+// function for email
+const validateEmail = (input, requiredMsg, invalidMsg) => {
+	// check if the value is not empty
+	if (!includeTheValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+    console.log(email)
+	if (!emailRegex.test(email)) {
+		return appearingError(input, invalidMsg);
+	}
+	return true;
+}
+
+//mobile phone function
+
+const validatePhone = (input, requiredMsg, invalidMsg) => {
+
+    if (!includeTheValue(input, requiredMsg)) {
+		return false;
+	}
+
+    let mobileRegex = /^[0-9]{11}$/;
+
+    let insertTelephone = input.value.trim();
+    console.log(insertTelephone)
+
+    if(!mobileRegex.test(insertTelephone)) {
+        appearingError(input, invalidMsg);
+    }
+    return true;
+  }
+
+  //validatePhone()
+  const vitalMobileNum = "Please insert your number";
+  const incorrectNum = "Please enter a correct number";
+
+  const emailAddressNeeded = "Please enter your email here";
+  const incorrectEmail = "Please enter a correct email address format";
+
+
+  // fuction to check if both mobile and email are valid, if not then throw and alert.
+  const checkConfirm = e => {
+  e.preventDefault();
+
+	// validate the form information for both number and email.
+	let numberValidation = validatePhone(confirmForm.elements["mobile-contact"], vitalMobileNum, incorrectNum);
+	let emailValidation = validateEmail(confirmForm.elements["email"], emailAddressNeeded, incorrectEmail);
+
+	// then if num and email are both valid then post an alert.
+	if (numberValidation && emailValidation) {
+		alert("Confirming and posting the information");
+	}
+  }
+
+  // handling the confirmation handler with an event listener on 'submit'.
+
+  confirmForm.addEventListener("submit", checkConfirm);
 
 
