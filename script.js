865597814d737241af99dbc80753288ad47db09d
PJSalter <psalter88@googlemail.com>
2022-03-14 02:37:09 +0000

    refine ES6 JS code

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-// ********** JavaScript code will go here ************* //
+// ********** JavaScript code ************* //
 
 // picking up the form element to start off with so that I can access the rest of the elements.
 const confirmForm = document.querySelector('#confirm');
-//console.log(confirmForm);
 
 const message = document.querySelector(".message")
 
 // show a message from a type of input, whether its the email or mobile.
 // create a function that will appear this message once user confirms there details.
-
 const popupMessage = (input, message, type) => {
     //picking up the <small> (small element) to pin a message
     // returning the parent of the particular node from the DOM tree.
     let messageBeep = input.parentNode.querySelector("small");
     messageBeep.innerText = message;
-    //console.log(messageBeep)
     // reforming the input class attribute
     input.className = type ? "success" : "error";
     return type;
 }
 
-// error will appear if information is incorrect
-
+// error will appear if information is incorrect.
 const appearingError = (input, message) => popupMessage(input, message, false);
-
-//console.log(appearingError)
 
 // congrats to the user for entering the correct validated info.
 const correctInfo = input => popupMessage(input, "", true);
-//console.log(correctInfo)
 
 // setting the value onto these inputs.
 const includeTheValue = (input, message) => {
@@ -41,8 +34,6 @@ if(input.value.trim() === "") {
     
 }
 }
-//console.log(input)
-
 // function for email
 const validateEmail = (input, requiredMsg, invalidMsg) => {
 	// check if the value is not empty
@@ -54,7 +45,6 @@ const validateEmail = (input, requiredMsg, invalidMsg) => {
 		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 	const email = input.value.trim();
-    //console.log(email)
 	if (!emailRegex.test(email)) {
 		return appearingError(input, invalidMsg);
 	}
@@ -62,7 +52,6 @@ const validateEmail = (input, requiredMsg, invalidMsg) => {
 }
 
 //mobile phone function
-
 const validatePhone = (input, requiredMsg, invalidMsg) => {
 
     if (!includeTheValue(input, requiredMsg)) {
@@ -80,13 +69,11 @@ const validatePhone = (input, requiredMsg, invalidMsg) => {
     return true;
   }
 
-  //validatePhone()
   const vitalMobileNum = "Please insert your number";
   const incorrectNum = "Please enter a correct number";
 
   const emailAddressNeeded = "Please enter your email here";
   const incorrectEmail = "Please enter a correct email address format";
-
 
   // fuction to check if both mobile and email are valid, if not then throw and alert.
   const checkConfirm = e => {
@@ -98,25 +85,13 @@ const validatePhone = (input, requiredMsg, invalidMsg) => {
    
     let bothAcceptable = numberValidation && emailValidation;
 
-
-    
     if(bothAcceptable != true) {
-        //alert("Received your contact information");
-        //let message = document.querySelector('input.success')
         message.innerText = "Incorrect details, please try again"
         message.style.color = "var(--error-red)"
     } else {
         message.innerText = "Received your contact information";
         message.style.color = "var(--correct-green)"
-    }
-
-    
-
-   
-   
+    } 
 }
   // handling the confirmation handler with an event listener on 'submit'.
-  
   confirmForm.addEventListener("submit", checkConfirm);
-//   line to make form disappear
-//   confirmForm.style.display = 'none';
